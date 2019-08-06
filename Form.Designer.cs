d1f1d644bdea4ff482975b69e2009739eed3574e
felipesja <felipesantos.uerj@gmail.com>
2019-08-05 22:05:07 -0300

    Adicionar arquivos de projeto.

---
+++ Form.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace WebScrapingSelenium {
+    partial class Form {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent() {
+            this.textBox = new System.Windows.Forms.RichTextBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnFileOpen = new System.Windows.Forms.Button();
+            this.pbForm = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // textBox
+            // 
+            this.textBox.Location = new System.Drawing.Point(12, 12);
+            this.textBox.Name = "textBox";
+            this.textBox.Size = new System.Drawing.Size(529, 555);
+            this.textBox.TabIndex = 0;
+            this.textBox.Text = "";
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(137, 603);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(86, 32);
+            this.btnStart.TabIndex = 1;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.BtnStart_Click);
+            // 
+            // btnFileOpen
+            // 
+            this.btnFileOpen.Location = new System.Drawing.Point(315, 603);
+            this.btnFileOpen.Name = "btnFileOpen";
+            this.btnFileOpen.Size = new System.Drawing.Size(86, 32);
+            this.btnFileOpen.TabIndex = 2;
+            this.btnFileOpen.Text = "Open File";
+            this.btnFileOpen.UseVisualStyleBackColor = true;
+            this.btnFileOpen.Click += new System.EventHandler(this.BtnFileOpen_Click);
+            // 
+            // pbForm
+            // 
+            this.pbForm.Location = new System.Drawing.Point(13, 574);
+            this.pbForm.Name = "pbForm";
+            this.pbForm.Size = new System.Drawing.Size(528, 23);
+            this.pbForm.Style = System.Windows.Forms.ProgressBarStyle.Continuous;
+            this.pbForm.TabIndex = 3;
+            // 
+            // Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(553, 647);
+            this.Controls.Add(this.btnFileOpen);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.pbForm);
+            this.Controls.Add(this.textBox);
+            this.Name = "Form";
+            this.Text = "Form";
+            this.Load += new System.EventHandler(this.Form_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox textBox;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnFileOpen;
+        private System.Windows.Forms.ProgressBar pbForm;
+    }
+}
+
